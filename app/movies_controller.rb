12d74b5137e7f1ea38d232b0d2e5fbc9cf9bced7
agdsarge <agdsargeant@gmail.com>
2020-04-30 17:43:21 -0400

    passed a few more tests

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -45,12 +45,11 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-    binding.pry
   Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find(1)
 end
 
 def can_find_by_multiple_attributes
@@ -58,17 +57,19 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
+
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  Movie.where("release_date > 2002").order(release_date: :desc)
+
 end
 
 def can_be_found_updated_and_saved
-  # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
+  # Update the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
   __
   __
